87b170237beb23dd9cb7e723fb7ede8c9867994e
ChronoLogic <msuttie12@hotmail.com>
2024-07-07 15:57:18 +0100

    Add countdown timer

---
+++ src/components/Countdown/index.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+import css from "./style.module.css";
+
+export default (props) => {
+	const calculateTimeRemaining = () => {
+		const difference = +new Date(props.endDate) - +new Date();
+		let timeRemaining = {};
+		if (difference > 0) {
+			timeRemaining = {
+				days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+				hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+				minutes: Math.floor((difference / 1000 / 60) % 60),
+				seconds: Math.floor((difference / 1000) % 60),
+			};
+		}
+		return timeRemaining;
+	};
+
+	const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setTimeRemaining(calculateTimeRemaining());
+		}, 1000);
+		return () => clearTimeout(timer);
+	});
+
+	const timerComponents = [];
+	Object.keys(timeRemaining).forEach((interval) => {
+		timerComponents.push(
+			<span className={css.time} key={interval}>
+				{timeRemaining[interval]} {interval}{" "}
+			</span>
+		);
+	});
+
+	return (
+		<div className={css.countdown}>
+			{timerComponents.length ? timerComponents : <p className={css.time}>EDF DEPLOY!</p>}
+		</div>
+	);
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
+import React, { useEffect, useState } from "react";
+
 import Head from "next/head";
 import styles from "../styles/Countdown.module.css";
 
+import { Countdown } from "../components";
+
 export default function Home() {
+	const [endDate, setEndDate] = useState("");
+
+	useEffect(() => {
+		setEndDate(new Date("july 25, 2024 00:00:00").getTime());
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -9,19 +19,16 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
+			<main className={styles.pageContainer}>
 				<section className={styles.headerContainer}>
 					<h1 className={styles.title}>EDF 6 Western PC Release</h1>
 					<h2 className={styles.date}>
 						<i>25 · 07 · 24</i>
 					</h2>
 				</section>
-				{/* <section className={styles.countdownContainer}>
-					<h1 className={styles.title}>EDF 6 Western PC Release</h1>
-					<h2 className={styles.date}>
-						<i>25 · 07 · 24</i>
-					</h2>
-				</section> */}
+				<section className={styles.countdownContainer}>
+					<Countdown endDate={endDate} setEndDate={setEndDate} />
+				</section>
 			</main>
 
 			<footer>
@@ -29,10 +36,10 @@ export default function Home() {
 					href="https://store.steampowered.com/app/2291060/EARTH_DEFENSE_FORCE_6/"
 					target="_blank"
 					rel="noopener noreferrer">
-					<i class="fa-brands fa-steam"></i> Steam
+					<i className="fa-brands fa-steam"></i> Steam
 				</a>
 				<a href="https://www.d3p.co.jp/edf6/en/" target="_blank" rel="noopener noreferrer">
-					<i class="fa-solid fa-circle-arrow-up"></i> Publisher (D3)
+					<i className="fa-solid fa-circle-arrow-up"></i> Publisher (D3)
 				</a>
 			</footer>
 
